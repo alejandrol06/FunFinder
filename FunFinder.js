42a1c2a79126d057f762ae2146b4a052cc24f02d
Alejandro López Vega <ale190402@gmail.com>
2017-12-28 14:56:01 +0100

    lots of changes (not finished)

--- FunFinder.js
+++ FunFinder.js
@@ -1,6 +1,7 @@
 <script>
 // Get the modal
-var modal = document.getElementById('login');
+var modal_login = document.getElementById('login');
+var modal_account = document.getElementById('createaccount');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
